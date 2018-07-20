9382568a750227bf2cb47e308a757e3a82b7d372
vano_q3 <vano271993@gmail.com>
2018-07-20 17:08:53 +0300

    Generated User controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {registrations: 'user/registrations', sessions: 'user/sessions'}
   resources :projects do
-  	resources :tasks
+  	resources :tasks do
+  		resources :users
+  	end
   end
+
   root 'projects#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

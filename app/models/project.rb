7bdb758b58e01b93e4ad9f75b70108b7cac52bf1
vano_q3 <vano271993@gmail.com>
2018-07-23 12:01:54 +0300

    Add validation. Change css

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,4 @@
 class Project < ApplicationRecord
 	has_many :tasks
+	validates :name, :summary, :start_date, :end_date, presence: true
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -3,4 +3,5 @@ class Task < ApplicationRecord
   has_and_belongs_to_many :users
   status_options = ['new', 'in progress', 'done']
   validates_inclusion_of :status, :in => status_options
+  validates :title, :description, presence: true
 end
